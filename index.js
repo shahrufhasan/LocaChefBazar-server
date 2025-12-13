960b3bf0613f71fbebb3d1200086280962a5c36a
Shahruf hasan <info.shahrufhasan@gmail.com>
2025-12-12 21:03:57 -0500

    started with get api

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnv9fix.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // ALL THE API HERE
+    const db = client.db("LocalChefBazarDB");
+    const mealsCollection = db.collection("meals");
+
+    // meals api
+    app.get("/meals", async (req, res) => {
+      
+    });
+
+    app.post("/meals", async (req, res) => {
+      const meal = req.body;
+      const result = await mealsCollection.insertOne(meal);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello LocalChefBazar!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
